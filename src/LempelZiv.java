abf57f59140eece07997bb24b0d60c35b471794c
Matthew Askes <matthewaskes@gmail.com>
2017-06-02 19:44:21 +1200

    finished lempel ziv, now encodes and decodes properly

--- src/LempelZiv.java
+++ src/LempelZiv.java
@@ -1,48 +1,77 @@
-import java.util.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * A new instance of LempelZiv is created for every run.
  */
 public class LempelZiv {
+    private final int WINDOW_SIZE;
+    
+    /**
+     * creates a new instance with a given window size
+     * @param WINDOW_SIZE the size of the window
+     */
+    public LempelZiv(int WINDOW_SIZE) {
+        this.WINDOW_SIZE = WINDOW_SIZE;
+    }
+    
+    /**
+     * creates a new instance with a default window size of 100.
+     */
+    public LempelZiv(){
+        WINDOW_SIZE = 100;
+    }
+    
     /**
      * Take uncompressed input as a text string, compress it, and return it as a
      * text string.
      */
     public String compress(String input) {
-
-        StringBuilder out = new StringBuilder();
-        char[] charArray = input.toCharArray();
-        String behind;
-        int cursor = 0;
-        while (cursor < charArray.length) {
-            char c = charArray[cursor];
-            behind = (input.substring(0,cursor+1));
-            int curMatch = 0;
-            int curMatchLength = -1;
-            for (int i = cursor ; i < input.length(); i++) {
-                String search = input.substring(cursor, i);
-                int match = behind.indexOf(search);
-                int matchLength = search.length();
-                if (match >= 0 && matchLength > curMatchLength) {
-                    curMatchLength = matchLength;
-                    curMatch = match;
+        
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            char[] inputCharacters = input.toCharArray();
+            String behind;
+            outputStream.write(createTuple(0, 0, String.valueOf(inputCharacters[0])).getBytes());
+            int cursor = 1;
+            while (cursor < inputCharacters.length) {
+                behind = (input.substring(Math.max(0, cursor - WINDOW_SIZE), cursor));
+                int curMatch = 0;
+                int curMatchLength = 0;
+                for (int lookAHead = cursor + 1; lookAHead < Math.min(cursor + WINDOW_SIZE, input.length()); lookAHead++) {
+                    String search = input.substring(cursor, lookAHead);
+                    int match = behind.indexOf(search);
+                    int matchLength = search.length();
+                    if (match >= 0 && matchLength > curMatchLength) {
+                        curMatchLength = matchLength;
+                        curMatch = match;
+                    }
+                    if (match < 0) { // if a match is not found (speeds up the processing)
+                        break;
+                    }
+                }
+                if (curMatchLength == 0) {
+                    outputStream.write(createTuple(0, curMatchLength, String.valueOf(inputCharacters[cursor + curMatchLength])).getBytes());
+                } else
+                if (cursor + curMatchLength < inputCharacters.length) {
+                    outputStream.write(createTuple(curMatch - cursor + Math.max(0, cursor - WINDOW_SIZE), curMatchLength, String.valueOf(inputCharacters[cursor + curMatchLength])).getBytes());
+                }
+        
+                cursor += curMatchLength+1;
+                if (cursor >= inputCharacters.length - 1) {
+                    if (cursor < inputCharacters.length) {
+                        outputStream.write(createTuple(0, 0, String.valueOf(inputCharacters[cursor])).getBytes());
+                    }
+                    break;
                 }
             }
-            if (curMatch - cursor == 0) {
-                out.append(createTuple(curMatch - cursor, curMatchLength-1, String.valueOf(c)));
-            } else if (cursor+curMatchLength < charArray.length){
-                out.append(createTuple(curMatch - cursor, curMatchLength, String.valueOf(charArray[cursor+curMatchLength])));
-                cursor++;
-            }
-            
-            cursor += curMatchLength;
-            if (cursor >= charArray.length-1){
-                out.append(createTuple(0, 0, String.valueOf(charArray[cursor])));
-                break;
-            }
+    
+            return new String(outputStream.toByteArray());
+        }catch (IOException e){
+            e.printStackTrace();
+            return null;
         }
-
-        return out.toString();
        
     }
     
@@ -51,7 +80,7 @@ public class LempelZiv {
      * @return the tuple
      */
     public static String createTuple(int a, int b, String c){
-        return "["+a+","+b+","+c+"]";
+        return "["+a+"|"+b+"|"+c+"]";
     }
 
     /**
@@ -60,19 +89,17 @@ public class LempelZiv {
      */
     public String decompress(String compressed) {
         Scanner in = new Scanner(compressed);
-        in.useDelimiter("]\\[|\\[|\\]|,");
+        in.useDelimiter("]\\[|\\[|]|\\|");
         StringBuilder out = new StringBuilder();
         while (in.hasNext()){
             int index = out.length();
             int first = in.nextInt();
             int second = in.nextInt();
             String characters = in.next();
-            if (first == 0 && second == 0){
-                out.append(characters);
-            } else {
+            if (first != 0 && second != 0){
                 out.append(out.substring(index+first,index+first+second));
-                out.append(characters);
             }
+            out.append(characters);
         }
         return out.toString();
     }
@@ -89,7 +116,7 @@ public class LempelZiv {
 
     public static void main(String args[]){
         LempelZiv lempelZiv = new LempelZiv();
-        String compressed = lempelZiv.compress("What about a larger chunk");
+        String compressed = lempelZiv.compress("text, more text");
         System.out.println(compressed);
 //        Scanner in = new Scanner(compressed);
 //        in.useDelimiter("]\\[|\\[|]|,");
